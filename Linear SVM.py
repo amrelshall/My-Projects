6fb9713bc91550a2f2579b5aee7347ad62acfc7c
Amr Elshall <76405215+amrelshall@users.noreply.github.com>
2020-12-21 13:23:19 +0200

    Update Linear SVM.py

--- Linear SVM.py
+++ Linear SVM.py
@@ -6,11 +6,6 @@ import matplotlib.pyplot as plt
 from scipy.io import loadmat
 from sklearn import svm
 
-pd.set_option('display.notebook_repr_html', False)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', 150)
-pd.set_option('display.max_seq_items', None)
-
 #---------------------------------------------------------
 # functions
 def plotData(X, y ,S):
